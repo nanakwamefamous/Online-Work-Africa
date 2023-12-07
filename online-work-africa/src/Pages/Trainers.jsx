f06e9251476b7b573bf12098484cefd6ef77ef62
Prince Boateng Obeng <110475124+nanakwamefamous@users.noreply.github.com>
2023-12-07 23:00:28 +0000

    update

--- online-work-africa/src/Pages/Trainers.jsx
+++ online-work-africa/src/Pages/Trainers.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import "../styles/Trainers.css"
+import img from '../images/goal.png'
 
 export default function Trainers() {
   return (
     <div>
-      
+      <h1>hello</h1>
+      <img src={img} alt='you'/>
     </div>
   )
 }
