f18f96268c6fc63647bf0cf078f61f0dae44bd89
Prince Boateng Obeng <110475124+nanakwamefamous@users.noreply.github.com>
2023-12-07 18:07:53 +0000

    update

--- online-work-africa/src/Pages/Home.jsx
+++ online-work-africa/src/Pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import "../styles/Home.css"
-// import ImageSlider from "../components/ImageSlider"
+import ImageSlider from "../components/ImageSlider"
 
 
 export default function Home() {
   return (
     <div>
-
+      <ImageSlider />
     </div>
   )
 }
---
+++ online-work-africa/src/slider-data.jsx
@@ -0,0 +1,17 @@
+export const sliderData = [
+  {
+    image: "./images/Slider 1.jpg",
+    heading: "Slide One",
+    desc: "This is the description of slide one Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+  },
+  {
+    image: "./images/Slider 2.jpg",
+    heading: "Slide Two",
+    desc: "This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+  },
+  {
+    image: "./images/Slider 3.jpg",
+    heading: "Slide Three",
+    desc: "This is the description of slide three Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+  },
+];
