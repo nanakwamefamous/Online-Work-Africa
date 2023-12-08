3f1afcb3aeae417a91c06cafe37268f28b0a292e
Prince Boateng Obeng <110475124+nanakwamefamous@users.noreply.github.com>
2023-12-08 11:05:16 +0000

    update

--- online-work-africa/src/Pages/Home.jsx
+++ online-work-africa/src/Pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import "../styles/Home.css";
+import Slider from "../components/components/Slider";
 
 
 export default function Home() {
   return (
     <div>
-      
+      <Slider />
     </div>
   )
 }
---
+++ online-work-africa/src/components/components/sliderImage.js
@@ -0,0 +1,33 @@
+import First from "../../images/Slider 1.png";
+import Second from "../../images/Slider 2.png";
+import Third from "../../images/Slider 3.png";
+import Fourth from "../../images/Slider 4.png";
+import Fifth from "../../images/Slider 5.png";
+
+export default [
+  {
+    title: "First Slide",
+    description: "This is the first slider Image of our carousel",
+    urls: First,
+  },
+  {
+    title: "Second Slide",
+    description: "This is the second slider Image of our carousel",
+    urls: Second,
+  },
+  {
+    title: "Third Slide",
+    description: "This is the Third slider Image of our carousel",
+    urls: Third,
+  },
+  {
+    title: "Fourth Slide",
+    description: "This is the Fourth slider Image of our carousel",
+    urls: Fourth,
+  },
+  {
+    title: "Fifth Slide",
+    description: "This is the Fifth slider Image of our carousel",
+    urls: Fifth,
+  },
+];
